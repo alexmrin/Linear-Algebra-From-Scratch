69c0c18bef74135982461a33e023e2749153fc31
alexmrin <alex.matsumura@akane.waseda.jp>
2023-04-25 16:49:15 +0900

    added gramschmidt

--- Projection.py
+++ Projection.py
@@ -4,6 +4,9 @@ def projection(x, B):
     proj_matrix = np.dot(np.dot(B, np.linalg.inv(np.dot(np.transpose(B), B))), np.transpose(B))
     return np.dot(proj_matrix, x)
 
-B = np.array([1, 1]).reshape(2, 1)
-x = np.array([-14, 4]).reshape(2, 1)
+'''
+B = np.array([1, 0, 0, 1, 0, 0]).reshape(3, 2)
+print(B)
+x = np.array([-14, 4, 3]).reshape(3, 1)
 print(projection(x, B))
+'''
